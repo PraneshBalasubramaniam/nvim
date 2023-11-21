93494c7fab77ee189d1cf9cfd5964439a344b2be
Pranesh <pranesh.b.k@focuscrafttech.com>
2023-11-21 22:09:11 +0530

    copied and little modified from https://gitlab.com/linuxdabbler

---
+++ init.lua
@@ -0,0 +1,10 @@
+require('initial')
+require('settings')
+
+-- vim.cmd('colorscheme gruvbox')                       -- set colorscheme
+-- vim.cmd('filetype plugin on')                        -- set filetype 
+-- vim.cmd('set wildmenu')                              -- enable wildmenu
+
+-- require('statusline')
+require('keymap')
+-- require('automate')
